2829560371d1077284c24327d791cfcadac0a773
Anantha Kumaran <ananthakumaran@gmail.com>
2013-03-10 15:05:29 +0530

    bump version

--- jquery-doc.el
+++ jquery-doc.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2011 Anantha Kumaran.
 
 ;; Author: Anantha kumaran <ananthakumaran@gmail.com>
-;; Version: 1.7
+;; Version: 1.9
 ;; Keywords: docs, jquery
 
 ;; This program is free software: you can redistribute it and/or
